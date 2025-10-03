f63fa045a79cdfca8ad82f04a2842485e89bf46f
Sergio MT <sergio.mt88@gmail.com>
2025-10-03 11:56:37 +0200

    fix: use findByIdAndDelete and return clear JSON responses

--- server/routes/task/handlers/removeById.js
+++ server/routes/task/handlers/removeById.js
@@ -1,18 +1,17 @@
-const Task = require('../../../models/Task')
+const Task = require("../../../models/Task");
 
-function removeById (req,res) {
-
-  const { id } = req.params
+function removeById(req, res) {
+  const { id } = req.params;
 
   Task.findByIdAndDelete(id)
-    .then( doc => {
-      if (!doc) return res.status(404).json({ ok: false, message: 'Not found' })
-      res.json({ ok: true, deleted: true, doc })
+    .then((doc) => {
+      if (!doc)
+        return res.status(404).json({ ok: false, message: "Not found" });
+      res.json({ ok: true, deleted: true, doc });
     })
-    .catch( err => {
-      res.status(500).json({ ok: false, error: err.message || err })
-    })
-
+    .catch((err) => {
+      res.status(500).json({ ok: false, error: err.message || err });
+    });
 }
 
-module.exports = removeById
+module.exports = removeById;
